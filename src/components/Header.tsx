6ed3f68624de7c08e57e377670f43a7b0acf1b9c
fikireab-m <fikireab.mekuriaw@kgp2ssindia.in>
2023-11-06 19:30:30 +0300

    glass effect on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="flex items-center justify-center bg-transparent">
+    <header className="items-center bg-transparent">
       <nav className="shadow-xl my-4 p-4 px-8 flex gap-6 md:gap-8 lg:gap-12 max-w-lg bg-gray-900 rounded-full backdrop-blur-lg bg-opacity-30">
         <a href="#" className="text-sm font-semibold leading-6 text-white">
           About
