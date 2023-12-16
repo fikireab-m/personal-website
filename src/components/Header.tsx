26af90bf9eb70c7e037ed56a80a146270f5db7e2
fikireab-m <fikireab.mekuriaw@kgp2ssindia.in>
2023-12-16 16:51:46 +0300

    mobile nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,54 @@
+import { useState } from "react";
+import menuIcon from "../assets/icons/menu-icon.svg"
+import closeIcon from "../assets/icons/close-icon.svg"
+
 const Header = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const [menuOpen, setMenuOpen] = useState(false)
+  const menueItems = ["About", "Services", "Contact", "View CV"]
+  function changeIndex(i: number) {
+    setActiveIndex(i);
+  }
   return (
     <header className="absolute z-10 left-0 right-0 top-0 flex items-center justify-center bg-transparent">
-      <nav className="shadow-xl my-4 p-4 px-8 flex gap-4 md-gap-8 lg-gap-12 items-center max-w-lg bg-gray-900 rounded-full backdrop-blur-lg bg-opacity-30">
-        <a href="#" className="text-sm font-semibold leading-6 text-white">
-          About
-        </a>
-        <a href="#" className="text-sm font-semibold leading-6 text-white">
-          Services
-        </a>
-        <a href="#" className="text-sm font-semibold leading-6 text-white">
-          Contact
-        </a>
-        <a href="#" className="text-sm font-semibold leading-6 text-white">
-          View CV
-        </a>
+      <nav className="block xs:hidden w-full flex p-4 bg-black backdrop-blur-lg bg-opacity-30">
+        {menuOpen
+          ? <div className="w-full flex justify-between items-start">
+            <div className="w-full p-4 flex flex-col gap-4 items-start justify-start">
+              {
+                menueItems.map((item, index) => (
+                  <button onClick={() => changeIndex(index)}>
+                    <a key={index} href="#" className={`${index === activeIndex ? "text-lg bg-teal-600 text-gray-200" : "bg-slate-900 bg-opacity-30"} py-2 px-4 font-semibold leading-6 text-gray-200 rounded-full`}>
+                      {item}{index === activeIndex && <span className="text-gray-400 pl-1">/</span>}
+                    </a>
+                  </button>
+                ))
+              }
+            </div>
+            <button onClick={() => setMenuOpen(false)}>
+              <img className="w-6 w-6" src={closeIcon} alt="menu icon" />
+            </button>
+          </div>
+          :
+          <>
+            <div className="flex-1" />
+            <button onClick={() => setMenuOpen(true)}>
+              <img className="w-6 w-6" src={menuIcon} alt="menu icon" />
+            </button>
+          </>
+        }
+
+      </nav>
+      <nav className="hidden xs:block shadow-xl my-4 p-4 md:px-4 flex items-center justify-around max-w-xl bg-black rounded-full backdrop-blur-lg bg-opacity-30">
+        {
+          menueItems.map((item, index) => (
+            <button onClick={() => changeIndex(index)} className="mx-1 sm:mx-2">
+              <a key={index} href="#" className={`${index === activeIndex ? "text-lg bg-teal-600 text-gray-200" : "bg-slate-900 bg-opacity-30"} py-2 px-4 font-semibold leading-6 text-gray-200 rounded-full`}>
+                {item}{index === activeIndex && <span className="text-gray-400 pl-1">/</span>}
+              </a>
+            </button>
+          ))
+        }
       </nav>
     </header>
   )
--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,6 +1,10 @@
 const Hero = () => {
   return (
-    <div className="font-mono w-full h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-teal-600 via-gray-900 to-black">Hero</div>
+    <div className="w-full h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-teal-600 via-gray-900 to-black text-lime-100 flex justify-center items-center">
+      <div className="w-full lg:w-4/5 xl:w-3/5 h-1/2 px-1 text-center">
+        <h1 className="text-6xl md:text-8xl font-black leading-relaxed">Hi. I'm Fikireab. <br /> A software engineer.</h1>
+      </div>
+    </div>
   )
 }
 
