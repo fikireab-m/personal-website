06258c843c38c6f360e5b90306a4dc459567ab52
fikireab-m <fikireab.meku@gmail.com>
2023-12-16 21:26:32 +0300

    about page

---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="w-full h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-teal-600 via-gray-800 to-black text-lime-100 flex justify-center items-center">
+        
+    </div>
+  )
+}
+
+export default About
