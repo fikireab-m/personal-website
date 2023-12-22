1637153b6e1483b10e5c5fecc7492cca5e4c852c
fikireab-m <fikireab.meku@gmail.com>
2023-12-22 20:52:31 +0300

    framer-motion

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,12 +1,14 @@
+import { motion } from "framer-motion"
+
 const Hero = () => {
   return (
-    <div className="w-full h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-teal-600 via-gray-800 to-black text-lime-100 flex justify-center items-center">
+    <motion.div className="w-full h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-teal-600 via-gray-800 to-black text-lime-100 flex justify-center items-center">
       <div className="w-full lg:w-4/5 xl:w-3/5 h-1/2 px-1 text-center">
         <h1 className="text-5xl xs:text-6xl sm:text-7xl md:text-8xl font-black leading-relaxed">
           Hi. I'm Fikireab. <br /> A software engineer.
         </h1>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
