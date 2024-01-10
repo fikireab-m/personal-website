24d89423b2279934849214d8d2e143787c67ccaf
fikireab-m <fikireab.meku@gmail.com>
2024-01-10 12:30:10 +0300

    custome svg

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -9,11 +9,11 @@ const Hero = () => {
   }
   return (
     <LazyMotion features={domAnimation}>
-      <m.div id="home" className="w-full md:h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-from-blue-600  via-gray-900 to-slate-900 text-white flex justify-center md:items-center"
+      <m.div id="home" className="w-full select-none md:h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-from-blue-600  via-gray-900 to-slate-900 text-white flex justify-center md:items-center"
         animate={{ opacity: 1 }}>
-        <div className="w-full xl:w-10/12 py-16 lg:px-8 block md:flex gap-4 justify-center items-center">
+        <div className="w-full xl:w-10/12 py-16 lg:px-8 block md:flex justify-center items-center">
           <div className="w-full md:w-3/5 pt-16 flex flex-col justify-center items-center">
-            <motion.h1 className="bg-clip-text text-transparent bg-gradient-to-r from-slate-200 to-teal-400 text-5xl xs:text-5xl sm:text-6xl font-black leading-relaxed text-center"
+            <motion.h1 className="bg-clip-text text-transparent bg-gradient-to-r from-slate-200 to-gray-400 text-5xl xs:text-5xl sm:text-6xl font-black leading-relaxed text-center"
               initial={{ y: -1800, opacity: 0 }}
               animate={{ y: 0, opacity: 1.0 }}
               transition={{ duration: 1.0 }}
@@ -24,7 +24,7 @@ const Hero = () => {
                 </span>
               ))}
             </motion.h1>
-            <motion.h1 className="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-slate-400 text-5xl xs:text-5xl sm:text-6xl md:text-8xl font-black leading-relaxed text-center"
+            <motion.h1 className="bg-clip-text text-transparent bg-gradient-to-r from-gray-400 to-slate-400 text-5xl xs:text-5xl sm:text-6xl md:text-8xl font-black leading-relaxed text-center"
               initial={{ y: -1800, opacity: 0 }}
               animate={{ y: 0, opacity: 1.0 }}
               transition={{ delay: 0.2, duration: 1.0, ease: 'easeOut' }}
@@ -36,7 +36,7 @@ const Hero = () => {
               ))}
             </motion.h1>
           </div>
-          <motion.div className="w-full md:w-2/5" style={style}
+          <motion.div className="w-full md:w-2/5 bg-gradient-to-b from-slate-900 to-gray-600" style={style}
             initial={{ x: 1800, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ delay: 0.2, duration: 1.5, ease: 'easeOut' }}
