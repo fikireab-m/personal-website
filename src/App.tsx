8ad4a96a9dfa4129a5be849dd720c964a5d2f892
fikireab-m <fikireab.meku@gmail.com>
2024-01-11 23:29:09 +0300

    no need of  navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css'
-import Header from './components/Header'
 import Hero from './pages/Hero'
-import AboutMe from './pages/About'
 const App=()=> {
   return (
     <>
-    <Header/>
+    {/* <Header/> */}
     <Hero/>
-    <AboutMe/>
     </>
   )
 }
--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,75 +1,191 @@
 import open_laptop from "../assets/images/971.png";
+import { useMotionValue, motion, useSpring, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
+import cloudImg from "../assets/images/cloud.jpg";
 export default function Hero() {
   return (
-    <div id="home" className="bg-gray-900">
+    <div className="bg-gray-900">
       <div className="relative isolate px-6 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-20 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-40"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-slate-900 to-gray-600 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
-        </div>
-        <svg
-          viewBox="0 0 1024 1024"
-          className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
-          aria-hidden="true"
-        >
-          <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.2" />
-          <defs>
-            <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-              <stop stopColor="#7775D6" />
-              <stop offset={1} stopColor="#E935C1" />
-            </radialGradient>
-          </defs>
-        </svg>
-        <div className="mx-auto max-w-7xl px-8 py-24 grid grid-cols-1 md:grid-cols-2 items-center gap-8 mx-auto">
-          <div className="text-left">
+
+        <div className="mx-auto max-w-7xl px-8 py-8 grid grid-cols-1 md:grid-cols-5 items-center gap-8 mx-auto">
+          <div className="text-left col-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-transparent sm:text-9xl bg-gradient-to-br from-blue-600 to-purple-600" style={{ backgroundClip: "text" }}>
               Hi.
             </h1>
-            <h1 className="text-4xl font-bold tracking-tight text-transparent sm:text-6xl bg-gradient-to-br from-blue-600 to-purple-600" style={{ backgroundClip: "text" }}>
+            <h1 className="text-4xl font-bold tracking-tight text-gray-400 sm:text-4xl" style={{ backgroundClip: "text" }}>
               I'm Fikireab.
             </h1>
-            <h1 className="text-4xl font-bold tracking-tight text-transparent sm:text-6xl bg-gradient-to-br from-blue-600 to-purple-600" style={{ backgroundClip: "text" }}>
+            <h1 className="text-4xl font-bold tracking-tight text-gray-200 sm:text-4xl" style={{ backgroundClip: "text" }}>
               A software engineer.
             </h1>
             <p className="mt-6 text-xl font-thin leading-8 text-gray-200">
               Crafting Tomorrow's Solutions Today.<br /> Elevate Your Digital Experience with me.
             </p>
 
-            <button className="ml-32 mt-10 bg-gradient-to-r from-blue-600 to-indigo-600 hover:bg-blue-500 text-white font-bold py-3 px-8 lg:px-16 rounded-full shadow-md hover:text-white shadow-blue-300 transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
+            <button className="lg:ml-32 mt-10 bg-gradient-to-r from-blue-600 to-indigo-600 hover:bg-blue-500 text-white font-bold py-3 px-8 lg:px-16 rounded-full shadow-md hover:text-white shadow-blue-300 transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
               <a href="#">
                 Contact me
               </a>
             </button>
           </div>
-          <div className="max-w-xl pt-2 pr-2" >
-            <img src={open_laptop} alt="" style={{
-              mixBlendMode: "color-burn",
-              position: "relative",
-              overflow: "hidden"
-            }} />
+          <div className="col-span-3 pt-2 pr-2" >
+            <About/>
           </div>
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
         </div>
       </div>
     </div>
   )
 }
+
+
+
+
+
+export const About = () => {
+  return (
+    <section className="">
+      <div className="mx-auto max-w-5xl">
+        <Link
+          heading="About"
+          subheading="Learn what I do here"
+          imgSrc={cloudImg}
+          href="#"
+        />
+        <Link
+          heading="Experiance"
+          subheading="I work with great people"
+          imgSrc={open_laptop}
+          href="#"
+        />
+        <Link
+          heading="Portfolio"
+          subheading="My work speaks for itself"
+          imgSrc={cloudImg}
+          href="#"
+        />
+        <Link
+          heading="Contact"
+          subheading="Incase you're bored"
+          imgSrc={open_laptop}
+          href="#"
+        />
+      </div>
+    </section>
+  );
+};
+
+interface LinkProps {
+  heading: string;
+  imgSrc: string;
+  subheading: string;
+  href: string;
+}
+
+const Link: React.FC<LinkProps> = ({ heading, imgSrc, subheading, href }) => {
+  const ref = useRef<HTMLAnchorElement>(null);
+
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const mouseXSpring = useSpring(x);
+  const mouseYSpring = useSpring(y);
+
+  const top = useTransform(mouseYSpring, [0.5, -0.5], ["40%", "60%"]);
+  const left = useTransform(mouseXSpring, [0.5, -0.5], ["60%", "70%"]);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    const rect = ref.current?.getBoundingClientRect();
+
+    if (rect) {
+      const width = rect.width;
+      const height = rect.height;
+
+      const mouseX = e.clientX - rect.left;
+      const mouseY = e.clientY - rect.top;
+
+      const xPct = mouseX / width - 0.5;
+      const yPct = mouseY / height - 0.5;
+
+      x.set(xPct);
+      y.set(yPct);
+    }
+  };
+
+  return (
+    <motion.a
+      href={href}
+      ref={ref}
+      onMouseMove={handleMouseMove}
+      initial="initial"
+      whileHover="whileHover"
+      className="group relative flex items-center justify-between border-b-2 border-neutral-700 py-4 transition-colors duration-500 hover:border-neutral-50 md:py-8"
+    >
+      <div>
+        <motion.span
+          variants={{
+            initial: { x: 0 },
+            whileHover: { x: -16 },
+          }}
+          transition={{
+            type: "spring",
+            staggerChildren: 0.075,
+            delayChildren: 0.25,
+          }}
+          className="relative z-10 block text-4xl font-bold text-neutral-500 transition-colors duration-500 group-hover:text-neutral-50 md:text-6xl"
+        >
+          {heading.split("").map((l, i) => (
+            <motion.span
+              variants={{
+                initial: { x: 0 },
+                whileHover: { x: 16 },
+              }}
+              transition={{ type: "spring" }}
+              className="inline-block"
+              key={i}
+            >
+              {l}
+            </motion.span>
+          ))}
+        </motion.span>
+        <span className="relative z-10 mt-2 block text-base text-neutral-500 transition-colors duration-500 group-hover:text-neutral-50">
+          {subheading}
+        </span>
+      </div>
+
+      <motion.img
+        style={{
+          top,
+          left,
+          translateX: "-50%",
+          translateY: "-50%",
+        }}
+        variants={{
+          initial: { scale: 0, rotate: "-12.5deg" },
+          whileHover: { scale: 1, rotate: "12.5deg" },
+        }}
+        transition={{ type: "spring" }}
+        src={imgSrc}
+        className="absolute z-0 h-24 w-32 rounded-lg object-cover md:h-48 md:w-64"
+        alt={`Image representing a link for ${heading}`}
+      />
+
+      <motion.div
+        variants={{
+          initial: {
+            x: "25%",
+            opacity: 0,
+          },
+          whileHover: {
+            x: "0%",
+            opacity: 1,
+          },
+        }}
+        transition={{ type: "spring" }}
+        className="relative z-10 p-4"
+      >
+        <ArrowLongRightIcon className="text-5xl text-neutral-50" />
+      </motion.div>
+    </motion.a>
+  );
+};
