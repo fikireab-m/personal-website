e88f02f1fe7c703f819eb8576a8d270edb1a839a
fikireab-m <fikireab.meku@gmail.com>
2024-01-19 00:05:20 +0300

    some style fix

--- src/components/SocialBar.tsx
+++ src/components/SocialBar.tsx
@@ -2,12 +2,11 @@ import { FaceFrownIcon, GiftTopIcon, InboxStackIcon, LinkIcon } from "@heroicons
 import myPic from "../assets/images/picme_rounded.png"
 const SocialBar = () => {
     return (
-        <aside className="fixed z-10 top-0 right-0 left-0 flex items-center pt-4">
-            <a href="" className="lg:ml-16">
+        <aside className="fixed z-10 top-0 right-0 left-0 flex justify-between items-center pt-4 px-8 lg:px-16">
+            <a href="">
                 <img src={myPic} alt="My picture" className="w-12 h-12 rounded-full" />
             </a>
-            <div className="flex-1" />
-            <div className="flex gap-4 item-center lg:mr-16">
+            <div className="flex gap-4 items-center">
                 <LinkIcon className="w-8 h-8 text-sky-500" />
                 <GiftTopIcon className="w-8 h-8 text-sky-500" />
                 <FaceFrownIcon className="w-8 h-8 text-sky-500" />
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,14 +2,13 @@ import myPic from "../assets/images/picme.png"
 const AboutMe = () => {
     return (
         <div id="aboutme" className="bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-gray-900 via-slate-900 to-gray-900 lg:overflow-hidden lg:h-screen">
-            <div className="relative isolate px-6 lg:px-8 mx-auto max-w-7xl ">
+            <div className="relative isolate mx-auto max-w-7xl ">
                 <h1 className="text-4xl font-bold text-center text-transparent bg-gradient-to-br from-blue-600 to-purple-600" style={{ backgroundClip: "text" }}>
                     About Me
                 </h1>
-                <div className="px-8 py-8 grid grid-cols-1 md:grid-cols-2 content-center items-start gap-8 mx-auto">
-
+                <div className="px-2 sm:px-8 py-8 grid grid-cols-1 md:grid-cols-2 items-start gap-8 mx-auto md:mr-32">
                     <div className="md:text-left text-gray-400">
-                        <div className="rounded-lg p-4 hover:bg-[#00eeff18] hover:bg-opacity-25">
+                        <div className="rounded-lg p-4 hover:bg-[#00eeff18]">
                             <p className="text-justify leading-6">
                                 Hello, I'm <strong>Fikireab Mekuriaw</strong>, a seasoned software engineer dedicated to crafting exceptional digital experiences with over 4 years of experience. My journey began with a love for problem-solving and has evolved into a passion for turning ideas into elegant, efficient, and scalable solutions. I approach software engineering as an art form, blending creativity and precision to build impactful digital experiences. Adapting to the rapid pace of technology is a continuous thrill for me, and I thrive in collaborative environments where diverse perspectives converge to create solutions greater than the sum of their parts. Beyond the code, I love traveling and learning the languages and cultures of others, believing that a well-rounded life enhances creativity.
                             </p>
@@ -25,8 +24,8 @@ const AboutMe = () => {
                             <li>Methodologies</li>
                         </ul>
                     </div>
-                    <div className="col-span-1" >
-                        <div className="img-container w-96 h-96">
+                    <div>
+                        <div className="img-container max-w-96">
                             <img src={myPic} alt="My pic" />
                         </div>
                     </div>
