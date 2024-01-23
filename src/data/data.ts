59dff3cf81441eea6b26c7b53e392a04a30c69a6
fikireab-m <fikireab.meku@gmail.com>
2024-01-23 23:29:06 +0300

    data ++

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,3 +1,11 @@
+import tsIcon from "../assets/icons/ts.png"
+import jsIcon from "../assets/icons/js.png"
+import pythonIcon from "../assets/icons/python.png"
+import flutterIcon from "../assets/icons/flutter.png"
+import dartIcon from "../assets/icons/dart.png"
+import tailwindIcon from "../assets/icons/tailwind.png"
+import nodeIcon from "../assets/icons/nodejs.png"
+import mongodb from "../assets/icons/mingodb.png"
 import reactIcon from "../assets/icons/reactjs.png"
 
 export const aboutme = `Hello, I'm Fikireab Mekuriaw, a seasoned software engineer with over 4 years of experience dedicated to crafting exceptional digital products. My journey began with a love for problem-solving and has evolved into a passion for turning ideas into elegant, efficient, and scalable solutions. I approach software engineering as an art form, blending creativity and precision to build impactful digital experiences. Adapting to the rapid pace of technology is a continuous thrill for me, and I thrive in collaborative environments where diverse perspectives converge to create solutions greater than the sum of their parts. Beyond the code, I love traveling and learning the languages and cultures of others, believing that a well-rounded life enhances creativity.`
@@ -19,11 +27,11 @@ export const experiences: Experience[] = [
         date: "Nov 20,2023 - Present",
         desc: "",
         techstacks: [
-            { icon: reactIcon, title: "Typescript" },
+            { icon: tsIcon, title: "Typescript" },
             { icon: reactIcon, title: "React js" },
-            { icon: reactIcon, title: "Python" },
+            { icon: pythonIcon, title: "Python" },
             { icon: reactIcon, title: "Django" },
-            { icon: reactIcon, title: "Tailwind CSS" },
+            { icon: tailwindIcon, title: "Tailwind CSS" },
             { icon: reactIcon, title: "Material UI" },
             { icon: reactIcon, title: "ERP Next" },]
     },
@@ -34,15 +42,16 @@ export const experiences: Experience[] = [
         date: "Feb 06,2023 - Nov 15, 2023",
         desc: "",
         techstacks: [
-            { icon: reactIcon, title: "Flutter" },
-            { icon: reactIcon, title: "Typescript" },
+            { icon: flutterIcon, title: "Flutter" },
+            { icon: dartIcon, title: "Dart" },
+            { icon: tsIcon, title: "Typescript" },
             { icon: reactIcon, title: "React js" },
             { icon: reactIcon, title: "Redux js" },
-            { icon: reactIcon, title: "Tailwind CSS" },
+            { icon: tailwindIcon, title: "Tailwind CSS" },
             { icon: reactIcon, title: "Express js" },
-            { icon: reactIcon, title: "MongoDB" },
+            { icon: mongodb, title: "MongoDB" },
             { icon: reactIcon, title: "Firebase" },
-            { icon: reactIcon, title: "Node js" },]
+            { icon: nodeIcon, title: "Node js" },]
     },
     {
         title: "Front-end developer",
@@ -54,7 +63,7 @@ export const experiences: Experience[] = [
             { icon: reactIcon, title: "React js" },
             { icon: reactIcon, title: "Redux js" },
             { icon: reactIcon, title: "Material UI" },
-            { icon: reactIcon, title: "Tailwind CSS" },]
+            { icon: tailwindIcon, title: "Tailwind CSS" },]
     },
     {
         title: "Chatbot developer",
@@ -64,8 +73,9 @@ export const experiences: Experience[] = [
         desc: `Developed and implemented chatbot solutions using Python,React,ChatterBot and Dialogflow.
         Applied NLP techniques like tokenization, part-of-speech-tagging, named entity recognition, and other usefull techniques for effective user interaction. Collaborated with cross-functional teams to ensure user-friendly conversational flows. Integrated chatbots with the learnig management system for seamless communication. Conducted testing and debugging, ensuring a smooth user experience. Demonstrated adaptability and commitment to staying updated on evolving technologies.`,
         techstacks: [
-            { icon: reactIcon, title: "Python" },
-            { icon: reactIcon, title: "ChatBot" },
+            { icon: pythonIcon, title: "Python" },
+            { icon: jsIcon, title: "Javascript" },
+            { icon: reactIcon, title: "ChatterBot" },
             { icon: reactIcon, title: "Dialog Flow" },
             { icon: reactIcon, title: "React js" },]
     }
