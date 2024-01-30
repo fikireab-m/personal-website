ac3dbebe8742c1bab0430f03b40d33f33437ac70
fikireab-m <fikireab.meku@gmail.com>
2024-01-30 18:57:15 +0300

    Serivices

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,10 @@
+import { BsFront } from "react-icons/bs"
 import myPic from "../assets/images/picme.png"
 import { aboutme } from "../data/data"
-import { FiCreditCard, FiMail, FiUser, FiUsers } from "react-icons/fi";
+import { IconType } from "react-icons"
+import { FaLaptopCode } from "react-icons/fa6"
+import { GrSettingsOption } from "react-icons/gr"
+import { FaMobileAlt } from "react-icons/fa"
 
 const AboutMe = () => {
     return (
@@ -37,18 +41,18 @@ const ServiceCards = () => {
             </h1>
             <div className="grid gap-4 grid-cols-2 lg:grid-cols-4">
                 <Card
-                    title="Account"
-                    subtitle="Manage profile"
+                    title="Front-end Development"
+                    subtitle="I create stunning UIs using various technologies"
                     href="#"
-                    Icon={FiUser}
+                    Icon={FaLaptopCode}
                 />
-                <Card title="Email" subtitle="Manage email" href="#" Icon={FiMail} />
-                <Card title="Team" subtitle="Manage team" href="#" Icon={FiUsers} />
+                <Card title="Backend Development" subtitle="I build fast and scalable systems" href="#" Icon={GrSettingsOption} />
+                <Card title="Mobile aApplication Development" subtitle="Beautiful cross platform mobile applications for business" href="#" Icon={FaMobileAlt} />
                 <Card
-                    title="Billing"
-                    subtitle="Manage cards"
+                    title="UI/UX Design"
+                    subtitle="I create beautful yet simplified user interfaces for your users"
                     href="#"
-                    Icon={FiCreditCard}
+                    Icon={BsFront}
                 />
             </div>
         </div>
@@ -57,23 +61,23 @@ const ServiceCards = () => {
 interface CardProps {
     title: string,
     subtitle: string,
-    Icon: unknown,
+    Icon: IconType,
     href: string
 }
 const Card = ({ title, subtitle, Icon, href }: CardProps) => {
     return (
         <a
             href={href}
-            className="w-full p-4 rounded border-[1px] border-slate-300 relative overflow-hidden group bg-gray-800"
+            className="w-full p-4 rounded relative overflow-hidden group bg-gray-800"
         >
-            <div className="absolute inset-0 bg-gradient-to-r from-violet-600 to-indigo-600 translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
+            <div className="absolute inset-0 bg-gradient-to-r from-sky-500 to-indigo-600 translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
 
-            <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-100 group-hover:text-violet-400 group-hover:rotate-12 transition-transform duration-300" />
+            <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-400 group-hover:text-slate-100 group-hover:rotate-12 transition-transform duration-300" />
             <Icon className="mb-2 text-2xl text-violet-600 group-hover:text-white transition-colors relative z-10 duration-300" />
-            <h3 className="font-medium text-lg text-slate-950 group-hover:text-white relative z-10 duration-300">
+            <h3 className="font-medium text-lg text-gray-400 group-hover:text-white relative z-10 duration-300">
                 {title}
             </h3>
-            <p className="text-slate-400 group-hover:text-violet-200 relative z-10 duration-300">
+            <p className="text-gray-400 group-hover:text-violet-200 relative z-10 duration-300">
                 {subtitle}
             </p>
         </a>
